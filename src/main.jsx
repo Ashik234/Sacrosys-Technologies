86b93137949ee798e41fd78416c71d84ef3a6a51
Ashik k <ashikash796@gmail.com>
2024-01-05 19:39:42 +0530

    refactor(Login):Implemented Toasify

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {QueryClient,QueryClientProvider,useQuery,} from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 const queryClient = new QueryClient();
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <App />
-    </QueryClientProvider>
-
-)
+    <ToastContainer />
+  </QueryClientProvider>
+);
